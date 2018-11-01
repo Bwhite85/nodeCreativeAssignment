7079d05b4a1351e6347ade046df735075bc681a6
EC2 Default User <ec2-user@ip-172-31-14-244.us-east-2.compute.internal>
2018-11-01 18:17:13 +0000

    first commit

---
+++ recipe/public/javascripts/app.js
@@ -0,0 +1,44 @@
+var app = window.angular.module('app', []);
+
+app.factory('recipeFetcher', recipeFetcher);
+app.controller('mainCtrl', mainCtrl);
+
+function recipeFetcher($http) {
+
+  var API_ROOT = 'recipe';
+  return {
+    get: function() {
+      return $http
+        .get(API_ROOT)
+        .then(function(resp) {
+          return resp.data;
+        });
+    }
+  };
+
+}
+
+function mainCtrl($scope, recipeFetcher,$http) {
+
+  $scope.recipe = [];
+
+  recipeFetcher.get()
+    .then(function(data) {
+      $scope.recipe = data;
+    });
+
+  $scope.addRecipe = function() {
+    var formData = { name: $scope.Name, imageUrl: $scope.Url, instructions: $scope.Instructions, ingredients: $scope.Ingredients };
+    console.log(formData);
+    var recipeURL = 'recipe';
+    $http({
+      url: recipeURL,
+      method: "POST",
+      data: formData
+    }).success(function(data, status, headers, config) {
+      console.log("Post worked");
+    }).error(function(data, status, headers, config) {
+      console.log("Post failed");
+    });
+  };
+}
