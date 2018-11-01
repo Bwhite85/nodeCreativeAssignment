fa7d88be19f9cdb08f38be0fbd5ace5d88cc645e
EC2 Default User <ec2-user@ip-172-31-14-244.us-east-2.compute.internal>
2018-11-01 19:31:03 +0000

    functionality works

--- recipe/routes/index.js
+++ recipe/routes/index.js
@@ -13,20 +13,20 @@ router.get('/', function(req, res) {
 var recipe = [
   {
     name: 'Basil Cream Chicken',
-    imageUrl: 'img/bg-img/skillet-basil-cream-chicken-6-of-6.jpg',
+    imageUrl: 'images/img/bg-img/skillet-basil-cream-chicken-6-of-6.jpg',
     instructions: 'Place milk and breadcrumbs in seperate bowl',
     ingredients: 'chicken breast and breadcrumbs'
   },
   {
     name: 'Ham Fried Rice',
-    imageUrl: 'img/bg-img/ham_fried_rice.jpg',
+    imageUrl: 'images/img/bg-img/ham_fried_rice.jpg',
     instructions: 'Cool rice',
     ingredients: 'rice'
 
   },
   {
     name: 'Green Chile Chicken Enchiladas',
-    imageUrl: 'img/bg-img/creamyGreenChileChickenEnchiladas.jpg',
+    imageUrl: 'images/img/bg-img/creamyGreenChileChickenEnchiladas.jpg',
     instructions: 'Cook Enchiladas',
     ingredients: 'enchiladas'
   }
